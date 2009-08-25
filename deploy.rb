b06b26a49e42aa54b03b41e0c3c60cdaabfac16f
carr <tomislav@infinum.hr>
2009-08-25 13:17:12 +0200

    Dodao deploy skriptu

---
+++ deploy.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# simple deploy script
+
+require "rubygems"
+require "thor"
+require "net/ssh"
+
+class Deploy < Thor
+
+	desc "deploy", "Easycluster deploy script"
+
+	def push
+
+		deploy_ip 				= 'iss.infinum.hr'
+		deploy_user 			= 'www-data'
+		deploy_port 			= '22'
+		deploy_path				= '/var/www/s/svn.funja.org'
+
+		Net::SSH.start("#{deploy_ip}", "#{deploy_user}", :port => "#{deploy_port}") do |ssh|
+			ssh.exec! "mkdir -p #{deploy_path}"
+			ssh.exec! "cd #{deploy_path}; git pull"
+			ssh.exec! "touch tmp/restart.txt"
+		end
+
+		puts "================================"
+		puts "       DEPLOY FINISHED!!!       "
+		puts "================================"
+	end
+end
+
+Deploy.start
+
